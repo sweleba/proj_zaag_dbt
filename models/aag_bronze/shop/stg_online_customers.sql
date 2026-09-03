3e3679e625e7b268793df51b075361ba22edda90
Loyiso Phura <phuthumaloyisopetse@gmail.com>
2026-09-03 10:00:55 +0000

    added jinja code for environment seperation

--- models/aag_bronze/shop/stg_online_customers.sql
+++ models/aag_bronze/shop/stg_online_customers.sql
@@ -1,5 +1,14 @@
-select 
-    id as customer_id
-,   first_name
-,   last_name
-from {{source('online','CUSTOMERS')}}
+{% if target.name!='prod' %}
+    select 
+        id as customer_id
+    ,   first_name
+    ,   last_name
+    from {{source('online_dev','CUSTOMERS')}}
+{%else%}
+    select 
+        id as customer_id
+    ,   first_name
+    ,   last_name
+    from {{source('online_prod','CUSTOMERS')}}
+{% endif %}
+
